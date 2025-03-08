abb79c7988d9c0c733e09dffa5bd0ab3cd78eaac
Murilo Barboza <murilobarboza04@gmail.com>
2025-03-08 11:23:52 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pyautogui 
+
+pyautogui.moveTo(1105, 447, 1)
+
+for i in range(10000):
+    if(pyautogui.position(1105, 447)):
+        pyautogui.moveTo(1200, 800, 1)
+        
+    if(pyautogui.position(1200, 800)):
+        pyautogui.moveTo(1105, 800, 1)
